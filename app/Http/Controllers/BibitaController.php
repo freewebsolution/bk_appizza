5a1b5ed414e37665c9fd0b12c7af7c5075ac1e00
Lucio <info@freewebsolution.it>
2022-03-26 09:21:46 +0100

    Realizza migrations Pizza,Insalatona,Bibita,Commenti

---
+++ app/Http/Controllers/BibitaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bibita;
+use Illuminate\Http\Request;
+
+class BibitaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Bibita  $bibita
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bibita $bibita)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Bibita  $bibita
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bibita $bibita)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Bibita  $bibita
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bibita $bibita)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Bibita  $bibita
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bibita $bibita)
+    {
+        //
+    }
+}
